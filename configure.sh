7dab58ad2643034f442b81d3f991ba061418e19e
Aidan Clyens <aidanclyens@gmail.com>
2026-06-12 00:33:21 -0400

    Add Dockerfile, configure script, and Docker helper scripts

---
+++ configure.sh
@@ -0,0 +1,24 @@
+set -eux;
+apt-get update;
+apt-get upgrade;
+apt-get install -y --no-install-recommends \
+  ca-certificates \
+  gpg \
+  wget \
+  build-essential \
+  net-tools \
+  cmake \
+  gdb \
+  git \
+  vim \
+  wget \
+  curl \
+  autotools-dev \
+  automake \
+  libc6-dbg \
+  openssh-client \
+  python3 \
+  python3-dev \
+  python3-pip \
+  python3-venv && \
+  rm -rf /var/lib/apt/lists/*;
---
+++ docker/linux/docker-build.sh
@@ -0,0 +1,2 @@
+docker build --platform=linux/arm64 \
+    -t 3dgraphicsengine-dev:latest .
